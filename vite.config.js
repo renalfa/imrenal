9e3ea06c8818f360c499b2fdb292c81ffb682b9e
rnlfzi <rfaidzilah@gmail.com>
2025-02-02 13:05:42 +0700

    change path image

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/firebase-storage": {
-        target: "https://firebasestorage.googleapis.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/firebase-storage/, ""),
-      },
-    },
-  },
 });
